24380ef2f64b240d3b7ab2b419e7e883eb078334
Christopher Carrasco <christopher.carrasco@outlook.com>
2025-03-18 17:18:53 +0100

    new com

---
+++ params.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+var=Bonjour
+echo $var
+echo $2 $2 $1
+echo $*
+echo $@
+# Les clauses * et @ selectionnent tous les arguments
+# Les clauses numériques représentent les arguments selon leur ordre

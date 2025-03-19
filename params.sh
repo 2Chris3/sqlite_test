a0184fe1b03955c4255e24466a4a4a8f55ffd95e
Christopher Carrasco <christopher.carrasco@outlook.com>
2025-03-19 14:17:26 +0100

    restart the project

--- params.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-var=Bonjour
-echo $var
-echo $2 $2 $1
-echo $*
-echo $@
-# Les clauses * et @ selectionnent tous les arguments
-# Les clauses numériques représentent les arguments selon leur ordre
